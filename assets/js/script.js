4c32e8835760349b8630cc4d9994563113fee0ec
Asnassta <79850172+Asnassta@users.noreply.github.com>
2022-05-24 19:56:00 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,59 @@ $(document).ready(function() {
   /*===============Modal=================*/
   $(".modal-open").on("click", function(event) {
     event.preventDefault();
-    $(".modal").fadeIn(333);
+    $(".modal").fadeIn(111);
  
     $('.modal').css('top', $(window).scrollTop()+'px');
-    $(".modal__inner").fadeIn(333);
+    $(".modal__inner").fadeIn(111);
     $('body').addClass("hidden");
   });
   $(".modal__close,  .closex").on("click", function(event) {
     event.preventDefault();
-    $(".modal").fadeOut('222');
+    $(".modal").fadeOut(111);
     $('body').removeClass("hidden");
   });
 /*==============/modal=================*/
 
-
+$('.copyright__year').html((new Date()).getFullYear());
 })
+
+
+$(function() {
+  $('form').submit(function(e) {
+    var $form = $(this);
+
+    
+    $.ajax({
+      type: 'POST',
+      url: 'mail.php',
+      data: $form.serialize()
+    }).done(function(e) {
+ 
+      if(e == 'error')
+      {
+        $form + $(' .form-contact__error').fadeIn(333);
+      }
+      else
+      {
+        $form + $(' .form-contact__result').fadeIn(222);
+        $('.modal-open').hide();
+        $('.btn-send__result').show();
+        // if($form.find($('input[name="modal"]')).val() == 1)
+       
+
+      }
+     
+    }).fail(function(e) {
+      $form + $(' .form-contact__error').fadeIn(333);
+    });
+
+    
+    //отмена действия по умолчанию для кнопки submit
+    e.preventDefault(); 
+  });
+});
+
+
+
+ 
+ 
